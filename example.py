6e3f86a8fb8304eea4c26e4135949281c66ce01d
teddowyn <git@teddowyn.de>
2021-01-13 11:25:14 +0100

    Add simple parser functions for restcalls to cs

--- example.py
+++ example.py
@@ -98,7 +98,7 @@ def runtime_service_example():
         sel = int(input("Enter a number: "))
     item = items[sel - 1]
 
-    act_service = cs.actvity_service
+    act_service = cs.activity_service
 
     class CustomSignalHandler(SignalHandler):
         def signal(self, msg: ExecutionMessage) -> bool:  # noqa: F811
@@ -107,6 +107,7 @@ def runtime_service_example():
             # os.kill(os.getpid(), signal.SIGINT)
 
     ac = act_service.start_sse(item, CustomSignalHandler())
+
     try:
         # Configuration values of the activity
         act_instance: ActivityInstance = ac.ssc.act_instance
